1a3d9bd5256cb9e605433e7e76818329f402f292
Florent Georges <fgeorges@fgeorges.org>
2019-11-04 17:46:01 +0100

    Commands to open files from Dired on Mac, using system apps.

--- init/mac.el
+++ init/mac.el
@@ -2,3 +2,24 @@
 (setq mac-command-modifier 'meta)
 ;; map the `option` key (both sides) to `C-`
 (setq mac-option-modifier 'control)
+
+;; common implementation for `fg:dired-mac-browser` and `fg:dired-mac-explore`
+(defun fg:dired-mac-broxplore (cmd)
+  (mapc
+   (lambda (path)
+     (shell-command
+      (concat cmd (shell-quote-argument path))))
+   (dired-get-marked-files)))
+
+;; equivalent to `dired-w32-browser` on Windows (open file in Dired using system app)
+(defun fg:dired-mac-browser ()
+  (interactive)
+  (fg:dired-mac-broxplore "open "))
+
+;; equivalent to `dired-w32explore` on Windows (open file in Dired using explorer/finder)
+(defun fg:dired-mac-explore ()
+  (interactive)
+  (fg:dired-mac-broxplore "open -R "))
+
+(define-key dired-mode-map (kbd "C-<return>") 'fg:dired-mac-explore)
+(define-key dired-mode-map (kbd "M-<return>") 'fg:dired-mac-browser)
